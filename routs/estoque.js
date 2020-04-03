452932bd0e815950b0687685f0492ca552ca9f79
Luiz <labm1997@gmail.com>
2020-04-03 20:40:55 -0300

    Grafico no tempo e ajuste de nome da tabela de historico para consumo

--- routs/estoque.js
+++ routs/estoque.js
@@ -4,6 +4,8 @@ const router = express.Router()
 // 
 // SELECT filamento.FILAMENTO_ID, (SELECT MASSA FROM USO WHERE USO.FILAMENTO_ID=filamento.FILAMENTO_ID ORDER BY (julianday('now')-julianday(DATA)) ASC LIMIT 1) as massaatual FROM filamento WHERE filamento.FILAMENTO_ID = 'BA16';
 // SELECT filamento.FILAMENTO_ID, (SELECT MASSA FROM USO WHERE USO.FILAMENTO_ID=filamento.FILAMENTO_ID AND julianday(DATA) < julianday('2019-11-18') ORDER BY (julianday('now')-julianday(DATA)) ASC LIMIT 1) as massaant FROM filamento WHERE filamento.FILAMENTO_ID = 'BA16';
+// SELECT *, (SELECT massa FROM HistoricoFilamentos as sub WHERE sub.FilamentoId=main.FilamentoId AND julianday(sub.DATA) < julianday(main.data) ORDER BY sub.data DESC LIMIT 1) - main.massa as massa_consumida FROM HistoricoFilamentos as main ORDER BY main.DATA DESC;
+// SELECT id, massa, data, SUM(massa) OVER ( ORDER BY data ASC, id ASC ) as cum FROM HistoricoFilamentos ORDER BY id;
 //
 
 router.get('/',(req,res)=>{
@@ -25,11 +27,12 @@ router.get('/gerenciar/atualizar',(req,res)=>{
     res.render("estoque/atualizar")
 })
 router.post('/gerenciar/atualizar', (req,res) => {
-    db.Filamento.findOne({attributes: ['id'], where: {codigo: req.body.codigo}}).then(filamento => {
+    db.Filamento.findOne({attributes: ['id', 'massa'], where: {codigo: req.body.codigo}}).then(filamento => {
         novamassa = parseInt(req.body.peso);
+        consumo = filamento.massa - novamassa;
 
-        db.HistoricoFilamento.create({
-            massa: novamassa,
+        db.ConsumoFilamento.create({
+            massa: consumo,
             data: new Date(),
             FilamentoId: filamento.id, 
         })
@@ -52,6 +55,17 @@ router.get('/gerenciar/cadrastar',(req,res)=>{
 })
 router.post('/gerenciar/cadrastar',(req,res)=>{
     res.render("estoque/cadrastar", {isok: true})
+})
+
+router.get('/gerenciar/getData', (req, res) => {
+    if(req.query["funcao"] == "consumo"){
+        dataInicial = req.query["dataInicial"];
+        dataFinal = req.query["dataFinal"];
+        db.ConsumoFilamento.obterConsumoAcumulado(dataInicial, dataFinal).then(data => res.send(data[0]));
+    }
+    else if(req.query["funcao"] == "massa"){
+        db.ConsumoFilamento.obterHistoricoMassa().then(data => res.send(data[0]));
+    }
 })
 
 router.get('/gerenciar/plotData', (req,res)=>{
